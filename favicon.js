c3e4bf06d1d5964653d8e8457ad9ea77daa52236
Zern <fahedalex419@gmail.com>
2025-05-13 11:20:46 -0400

    k

---
+++ favicon.js
@@ -0,0 +1,26 @@
+function createFavicon() {
+  const canvas = document.createElement('canvas');
+  canvas.width = 16;
+  canvas.height = 16;
+  const ctx = canvas.getContext('2d');
+  ctx.font = '12px sans-serif';
+  ctx.fillStyle = 'black';
+  let frame = 0;
+  const text = "Zern";
+
+  function drawFrame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillText(text.substring(0, frame), 0, 12);
+    frame = (frame + 1) % (text.length + 1);
+
+    const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = canvas.toDataURL("image/x-icon");
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
+
+  setInterval(drawFrame, 500);
+}
+
+createFavicon();
