56efbbdafb1d3d47630b367fcda8b84ca0e047fb
Zern <fahedalex419@gmail.com>
2025-05-13 16:04:50 -0400

    h

---
+++ latestVideo.js
@@ -0,0 +1,25 @@
+const API_KEY = 'AIzaSyBxHmrfHkP61yPp9ZhNTgwS0aBMcI5mQrs'; // dont worry this is an alt key and isnt connected to anything i use so have at it script kid :3
+const CHANNEL_ID = 'UC9nw8aKnMsplhps5xEe3BZA'; 
+
+async function fetchLatestVideo() {
+  const response = await fetch(
+    `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=1`
+  );
+  const data = await response.json();
+  const videoId = data.items[0].id.videoId;
+
+  const videoContainer = document.getElementById('latest-video');
+  videoContainer.innerHTML = `
+    <iframe 
+      width="560" 
+      height="315" 
+      src="https://www.youtube.com/embed/${videoId}" 
+      title="YouTube video player" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+      allowfullscreen>
+    </iframe>
+  `;
+}
+
+fetchLatestVideo();
