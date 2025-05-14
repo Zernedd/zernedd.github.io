85c7fad7286d38161ddec54f5b2d2c4d04886f90
Zern <fahedalex419@gmail.com>
2025-05-14 08:28:38 -0400

    gt

--- gallery.js
+++ gallery.js
@@ -5,6 +5,8 @@ const modalImage = document.getElementById('modal-image');
 const closeModal = document.querySelector('.close');
 const prevButton = document.getElementById('prev-button');
 const nextButton = document.getElementById('next-button');
+//DONT EVER DO ANYTHING LIKE THIS 
+// this is the only way i was able to get it to work
 
 const imageFolderPath = 'gallery/';
 const images = [
@@ -18,6 +20,13 @@ const images = [
     'Photo_25_05-13_09_42_27_26.png',
     'Screenshot 2025-05-06 114208.png',
     'Screenshot 2025-03-07 190434.png',
+    'Photo_25_05-05_20_58_12_66.png',
+    'Photo_25_05-06_13_45_30_45.png',
+    'Photo_25_05-06_13_45_40_39.png',
+    'Photo_25_05-06_14_51_50_69.png',
+    'Photo_25_05-06_15_26_48_48.png',
+    'Photo_25_05-06_19_43_53_76.png',
+    'Photo_25_05-06_21_12_18_10.png',
 ];
 let currentIndex = 0;
 
